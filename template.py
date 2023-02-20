d19535b43bb1d70a1150de09822b6db66b0c2d61
vivek mishra <starvivek1996@gmail.com>
2023-02-20 17:28:27 +0530

    first commit

---
+++ template.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s: ')
+
+package_name = "Sensor"
+
+list_of_files = [
+   ".github/workflows/main.yaml",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/components/__init__.py", 
+   f"src/{package_name}/utils/__init__.py", 
+   f"src/{package_name}/config/__init__.py", 
+   f"src/{package_name}/pipeline/__init__.py", 
+   f"src/{package_name}/entity/__init__.py", 
+   "requirements.txt", 
+   "setup.py",
+   "init_setup.sh"
+    
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # create an empty file
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
