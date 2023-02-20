69024555cb40aa402435f49ae67805992b0e521d
vivek mishra <starvivek1996@gmail.com>
2023-02-21 01:52:44 +0530

    model_trainer completed

--- Sensor/pipeline/training_pipeline.py
+++ Sensor/pipeline/training_pipeline.py
@@ -6,6 +6,7 @@ from Sensor.entity import config_entity
 from Sensor.components.data_ingestion import DataIngestion
 from Sensor.components.data_validation import DataValidation
 from Sensor.components.data_transformation import DataTransformation
+from Sensor.components.model_trainer import ModelTrainer
 
 
 
@@ -31,9 +32,11 @@ def start_training_pipeline():
         data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
         data_ingestion_artifact=data_ingestion_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
-        
-    except Exception as e:
-        raise SensorException(e, sys)
 
+        #model trainer
+        model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
+        
     except Exception as e:
         raise SensorException(e, sys)
